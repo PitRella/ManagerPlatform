dbee5683efcf7e948360ffe311eb39e8410515a4
PitRella <serezha.sliva@gmail.com>
2025-08-08 12:21:33 +0300

    feat(project): Add DashboardView for user project listing

--- project/views.py
+++ project/views.py
@@ -1,2 +1,29 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
+from django.db.models.query import QuerySet
 
+from project.models import Project
+
+
+class DashboardView(LoginRequiredMixin, ListView[Project]):
+    """
+    View for displaying a paginated list of projects on the dashboard.
+
+    Inherits from:
+        LoginRequiredMixin: Ensures that only authenticated users can access
+        ListView: Provides pagination and list display functionality
+
+    Attributes:
+        model: Project model used for queryset
+        template_name: Template used for rendering the view
+        context_object_name: Name used for the project list in template context
+        paginate_by: Number of projects displayed per page
+    """
+
+    model = Project
+    context_object_name = 'projects'
+    paginate_by = 10
+
+    def get_queryset(self) -> QuerySet[Project]:
+        """Returns projects for the current user."""
+        return Project.objects.for_user(self.request.user)
