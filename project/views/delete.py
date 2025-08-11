2625f84f0e2df2d4a5518ed5d6f67d17faa12968
PitRella <serezha.sliva@gmail.com>
2025-08-12 00:26:30 +0300

    feat(project): Remove CSRF protection decorator from ProjectDeleteView

--- project/views/delete.py
+++ project/views/delete.py
@@ -1,11 +1,8 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import DeleteView
 from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_protect
-from django.utils.decorators import method_decorator
 from project.models import Project
 
-@method_decorator(csrf_protect, name='dispatch')
 class ProjectDeleteView(LoginRequiredMixin, DeleteView):
     model = Project
     template_name = 'project/delete.html'
