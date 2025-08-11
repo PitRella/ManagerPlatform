cec053d7f3fff2a6bbf8a63e02899e9644384a4f
PitRella <serezha.sliva@gmail.com>
2025-08-11 11:27:50 +0300

    feat(project): Set app_name, registered url route for project creating

--- project/urls.py
+++ project/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from project.views import DashboardView
+from project.views.project_create import ProjectCreateView
 
+app_name = 'projects'
 urlpatterns = [
-    path('', DashboardView.as_view(), name='dashboard')
+    path('', DashboardView.as_view(), name='dashboard'),
+    path('/create', ProjectCreateView.as_view(), name='create')
 ]
