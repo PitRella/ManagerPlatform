446a3731075bc9fae8de182d877a8b87ac51b623
PitRella <serezha.sliva@gmail.com>
2025-08-11 23:09:39 +0300

    feat(project): Rename project_create view file to create

--- project/urls.py
+++ project/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from project.views import DashboardView
-from project.views.project_create import ProjectCreateView
+from project.views.create import ProjectCreateView
 
 app_name = 'projects'
 urlpatterns = [
