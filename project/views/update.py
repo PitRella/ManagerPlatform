80a490893c49caa85190a3bce969d31f3352c9c5
PitRella <serezha.sliva@gmail.com>
2025-08-12 08:28:50 +0300

    test(project): Convert unittest assertions to pytest assert statements

--- project/views/update.py
+++ project/views/update.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
+from django.template.loader import render_to_string
 from django.views.generic import UpdateView
 
 from core.mixins.views import HTMXResponseMixin
@@ -7,22 +8,25 @@ from project.forms import EditForm
 from project.models import Project
 
 
-class ProjectUpdateView(LoginRequiredMixin, HTMXResponseMixin, UpdateView):
-    """View for updating project titles via HTMX."""
-
+class ProjectUpdateView(
+    LoginRequiredMixin,
+    HTMXResponseMixin[Project],
+    UpdateView
+):
     model = Project
     form_class = EditForm
     template_name = 'project/edit.html'
 
     def get_queryset(self):
-        """Only projects for current user."""
         return Project.objects.for_user(self.request.user)
 
     def render_htmx_response(self, instance: Project) -> HttpResponse:
-        html = (
-            f'<h4 class="mb-0 project-title" '
-            f'hx-target="this" hx-swap="outerHTML" '
-            f'style="cursor: pointer;" '
-            f'title="Click to edit title">{instance.title}</h4>'
+        html = render_to_string(
+            'project/edit_title.html',
+            {
+                'project':
+                    instance
+            },
+            request=self.request
         )
         return HttpResponse(html)
