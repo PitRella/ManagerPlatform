ba22aa734ef3b9f6ac136ec53cb9fba26a82cdbf
PitRella <serezha.sliva@gmail.com>
2025-08-12 10:29:48 +0300

    feat(project): Add home view and update URL routing with authentication integration

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from project.views import HomeView
+
 urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
     path('dashboard/', include('project.urls')),
 ]
--- project/views/__init__.py
+++ project/views/__init__.py
@@ -1,11 +1,13 @@
 from .create import ProjectCreateView
 from .dashboard import DashboardView
 from .delete import ProjectDeleteView
+from .home import HomeView
 from .update import ProjectUpdateView
 
 __all__ = [
     'DashboardView',
     'ProjectCreateView',
     'ProjectDeleteView',
+    'HomeView',
     'ProjectUpdateView'
 ]
